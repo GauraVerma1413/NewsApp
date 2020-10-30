d571e49f5c173559d9f94f41dae16d8f9a00fde5
Gaurav verma <dornan998@gmail.com>
2020-10-30 11:34:15 +0530

    Setting Up Retrofit library and api fetching Done!!!

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsAPI.kt
@@ -0,0 +1,30 @@
+package com.androiddevs.mvvmnewsapp.api
+
+import com.androiddevs.mvvmnewsapp.ResponseObject
+import retrofit2.http.GET
+import retrofit2.http.Query
+import com.androiddevs.mvvmnewsapp.utils.Constants.Companion.API_KEY
+import retrofit2.Response
+
+interface NewsAPI {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("Country")
+        CountryCode : String = "us",
+        @Query("page")
+        pageNumber:Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ) : Response<ResponseObject>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery : String ,
+        @Query("page")
+        pageNumber : Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ) : Response<ResponseObject>
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY ="89abb130f683481db7072e342e274405"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
